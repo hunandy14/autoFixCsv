b3738ca1ca290ccf7c2376f6815f4172474c6845
Charlotte.Hong <hunandy14@gmail.com>
2022-09-17 00:13:53 +0900

    更新:: 1. 自動指定預設編碼，避免在不同環境下出錯 2. 新增去除數值多餘的空白

--- README.md
+++ README.md
@@ -3,5 +3,18 @@
 
 使用方法
 ```ps1
-irm bit.ly/3NbJmZV|iex; autoFixCsv "sample.csv"
+# 快速使用
+irm bit.ly/autoFixCsv|iex; autoFixCsv "sample.csv"
+```
+
+詳細用法
+```ps1
+# 載入函式庫
+irm bit.ly/autoFixCsv|iex;
+# 轉換並自動生成 sample1_fix.csv
+autoFixCsv 'sample1.csv'
+# 轉換並自動生成 sample1_fix.csv 且消除所有項目的前後空白
+autoFixCsv 'sample1.csv' -TrimValue
+# 轉換到 sample1_fix.csv
+autoFixCsv 'sample1.csv' 'sample1_fix.csv'
 ```
--- autoFixCsv.ps1
+++ autoFixCsv.ps1
@@ -1,12 +1,46 @@
+# 當前終端預設編碼文字 (Import-Csv等函式預設值)
+function DefEnc {
+    param(
+        [switch] $FullName
+    )
+    if (!$FullName) {
+        if ($PSVersionTable.PSVersion.Major -ge 7) { $Result = "UTF8" } else { $Result = "Default" }
+    } else {
+        (([System.Text.Encoding]::Default).EncodingName) -match '\((.*?)\)'|Out-Null
+        $Result = $matches[1]
+    } return $Result
+} # DefEnc -FullName
+
+
+# 自動修復CSV檔案格式
 function autoFixCsv {
     param (
         [Parameter(Position = 0, ParameterSetName = "", Mandatory)]
         [string] $Path,
         [Parameter(Position = 1, ParameterSetName = "")]
-        [string] $Destination
+        [string] $Destination,
+        [switch] $TrimValue
     )
-    if (!$Destination) { $Destination = $Path }
-    (Import-Csv $Path) | Export-Csv $Destination -NoTypeInformation
-} 
-# autoFixCsv 'sample2.csv' 'out.csv'
-# autoFixCsv 'out.csv'
+    # 檢查
+    if (!(Test-Path -PathType:Leaf $Path)) { throw "Input file does not exist" }
+    $File = Get-Item $Path
+    if (!$Destination) { $Destination = $File.BaseName + "_fix" + $File.Extension }
+
+    # 轉換CSV檔案
+    $CSV = (Import-Csv $Path -Encoding:(DefEnc))
+    if ($TrimValue) { # 消除多餘空白
+        foreach ($Item in $CSV) {
+            ($Item.PSObject.Properties)|ForEach-Object{ $_.Value = ($_.Value).trim() }
+        }
+    } $CSV|Export-Csv $Destination -Encoding:(DefEnc) -NoTypeInformation
+
+    # 輸出訊息
+    $EncName = DefEnc -FullName
+    Write-Host "From [$EncName]::" -NoNewline
+    Write-Host $Path -NoNewline -ForegroundColor:White
+    Write-Host " convert to"
+    Write-Host "   └─[$EncName]::" -NoNewline
+    Write-Host $Destination -ForegroundColor:Yellow
+} # autoFixCsv 'sample1.csv'
+# autoFixCsv 'sample1.csv' -TrimValue
+# autoFixCsv 'sample1.csv' 'sample1_fix.csv'
